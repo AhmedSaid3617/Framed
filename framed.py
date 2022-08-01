f05d0d91333ebf66ff7dc00e8fe62cd8718dec25
AhmedSaid3617 <said.ahmed3617@gmail.com>
2022-08-01 21:54:13 +0200

    Added play again button.

--- framed.py
+++ framed.py
@@ -1,72 +1,98 @@
-from tkinter import *
-from PIL import ImageTk, Image
-import os
+import random
+
 from framed_functions import *
 
-# Constants
-NORM_BUTTON_COLOUR = "#0389ff"
-CLICKED_BUTTON_COLOUR = "#79fcf8"
-GUESSES_COLOUR = "#fc3d8d"
-FONT = "serilic"
-X_BUTTON_START = 284
-PICTURE_DIRECTORY = os.path.dirname(os.path.realpath(__file__)) + "\images"
-# Define window.
-root = Tk()
-root.title("Framed")
-root.geometry("967x750")
+movie_index = 0
+movies = []
 
-# Define
-#  List of images
-image_dirs = os.listdir(PICTURE_DIRECTORY)
-pictures = []
-for img in image_dirs:
-    pictures.append(ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\" + img)))
+def main():
+    global movie_index
+    with open("E:\Coding\PythonProjects\Framed\\sol.txt", newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            movies.append(row)
+        if movie_index > len(movies):
+            movie_index =0
+        chosen_row = movies[movie_index]
+        movie_index += 1
 
-#  Label for display of images
-img = ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\001.jpeg"))
-img_label = Label(image=img)
+    PICTURE_DIRECTORY = os.path.dirname(os.path.realpath(__file__)) + f"\images\\{chosen_row[0]}"
+    MOVIE_NAME = chosen_row[1]
 
-#  Entry to enter movie name.
-e = Entry(width=50, bd=4, font=FONT)
+    # Define window.
+    global root
+    root = Tk()
+    root.title("Framed")
+    root.geometry("967x750")
 
-#  Buttons
-button_1 = Button(text="1", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
-                  command=lambda: switch_img(0, img_label, pictures))
-button_2 = Button(text="2", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
-                  command=lambda: switch_img(1, img_label, pictures))
-button_3 = Button(text="3", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
-                  command=lambda: switch_img(2, img_label, pictures))
-button_4 = Button(text="4", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
-                  command=lambda: switch_img(3, img_label, pictures))
-button_5 = Button(text="5", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
-                  command=lambda: switch_img(4, img_label, pictures))
-button_6 = Button(text="6", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
-                  command=lambda: switch_img(5, img_label, pictures))
+    # Define
+    #  List of images
+    image_dirs = os.listdir(PICTURE_DIRECTORY) # List of paths to images.
+    pictures = [] # List of ImageTk objects.
+    for img in image_dirs:
+        pictures.append(ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\" + img)))
 
-button_submit = Button(text="Submit", bg=NORM_BUTTON_COLOUR, font=10, activebackground=CLICKED_BUTTON_COLOUR,
-                       command=lambda: guess(buttons_list, 284,
-                                             img_label, pictures,
-                                             e, guesses_label,
-                                             button_submit))
+    #  Label for display of images
+    img = ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\001.jpeg"))
+    img_label = Label(image=img)
 
-buttons_list = [[button_2, 1], [button_3, 2], [button_4, 3], [button_5, 4], [button_6, 5]]
+    #  Entry to enter movie name.
+    e = Entry(width=50, bd=4, font=FONT)
 
-# Guesses left label
-guesses_label = Label(text="You have 6 guesses left!", font=FONT, fg=GUESSES_COLOUR)
+    #  Buttons
+    button_1 = Button(text="1", bg=CURRENT_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
+                      command=lambda: switch_img(0, img_label, pictures, button_1, full_buttons_list))
+    button_2 = Button(text="2", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
+                      command=lambda: switch_img(1, img_label, pictures, button_2, full_buttons_list))
+    button_3 = Button(text="3", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
+                      command=lambda: switch_img(2, img_label, pictures, button_3, full_buttons_list))
+    button_4 = Button(text="4", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
+                      command=lambda: switch_img(3, img_label, pictures, button_4, full_buttons_list))
+    button_5 = Button(text="5", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
+                      command=lambda: switch_img(4, img_label, pictures, button_5, full_buttons_list))
+    button_6 = Button(text="6", bg=NORM_BUTTON_COLOUR, activebackground=CLICKED_BUTTON_COLOUR, font=10,
+                      command=lambda: switch_img(5, img_label, pictures, button_6, full_buttons_list))
 
-# Display
-#  Label
-img_label.grid(row=0, column=0, columnspan=3)
+    button_submit = Button(text="Submit", bg=NORM_BUTTON_COLOUR, font=10, activebackground=CLICKED_BUTTON_COLOUR,
+                           command=lambda: guess(buttons_list,
+                                                 img_label, pictures,
+                                                 e, guesses_label,
+                                                 button_submit, full_buttons_list, MOVIE_NAME))
 
-#  Entry
-e.place(x=134, y=550, width=700, height=50)
+    button_replay = Button(text="PLay Again", bg=NORM_BUTTON_COLOUR, font=10, activebackground=CLICKED_BUTTON_COLOUR,
+                           command=lambda: [root.destroy(), main()])
 
-#  Button
-button_submit.place(x=845, y=550, width=80, height=50)
-button_1.place(x=X_BUTTON_START, y=620, width=50, height=50)
-x_button = X_BUTTON_START
+    buttons_list = [[button_2, 1], [button_3, 2], [button_4, 3], [button_5, 4], [button_6, 5]]
+    full_buttons_list = [button_1, button_2, button_3, button_4, button_5, button_6]
 
-# Guesses label
-guesses_label.place(x=359, y=700, width=250, height=50)
+    # Guesses left label
+    guesses_label = Label(text="You have 6 guesses left!", font=FONT, fg=GUESSES_COLOUR)
 
-root.mainloop()
+
+    # Display
+    #  Label
+    img_label.grid(row=0, column=0, columnspan=3)
+
+    #  Entry
+    e.place(x=134, y=550, width=700, height=50)
+
+    #  Button
+    button_submit.place(x=845, y=550, width=80, height=50)
+    button_1.place(x=X_BUTTON_START, y=620, width=50, height=50)
+    x_button = X_BUTTON_START
+    button_replay.place(x=845, y=620, width=120, height=50)
+
+
+    # Guesses label
+    guesses_label.place(x=359, y=700, width=250, height=50)
+
+    root.mainloop()
+
+
+# Python program to use
+# main for function call.
+if __name__ == "__main__":
+    main()
+
+
+
--- framed_functions.py
+++ framed_functions.py
@@ -1,34 +1,57 @@
-import tkinter.constants
 from tkinter import *
 from PIL import ImageTk, Image
 import os
+import random
+import csv
 
-CLICKED_BUTTON_COLOUR = "#79fcf8"
-MOVIE_NAME = "Knives Out"
+
+movie_dirs = os.listdir(os.path.dirname(os.path.realpath(__file__)) + "\images")
+
+# Constants
+NORM_BUTTON_COLOUR = "#0342ff"
+CLICKED_BUTTON_COLOUR = "#03ffee"
+CURRENT_BUTTON_COLOUR = "#03c4ff"
 GUESSES_COLOUR = "#fc3d8d"
 FONT = "serilic"
+X_BUTTON_START = 284
+
+
+NUMBERS_DICT = {
+    1: "first",
+    2: "second",
+    3: "third",
+    4: "fourth",
+    5: "fifth",
+    6: "sixth"
+}
 
 guesses = 6
 
 
-def switch_img(index, image_label, lst):
+def switch_img(index, image_label, lst, button, button_list):
     image_label.forget()
     image_label = Label(image=lst[index])
     image_label.grid(row=0, column=0, columnspan=3)
+    for b in button_list:
+        b["bg"] = NORM_BUTTON_COLOUR
+    button["bg"] = CURRENT_BUTTON_COLOUR
 
 
-def guess(button_list, x_start, image_label, img_list, entry, guess_label, submit_button):
+def guess(button_list, image_label, img_list, entry, guess_label, submit_button, full_b_list, movie):
     global guesses
-    if button_list:
-        if entry.get().lower() == MOVIE_NAME.lower():
-            guess_label = Label(text="Correct!", font=FONT, fg=GUESSES_COLOUR)
+    if button_list or entry.get().lower() == movie.lower():
+        if entry.get().lower() == movie.lower():
+            guess_label = Label(text=f"You got it on the {NUMBERS_DICT[7 - guesses]} guess!", font=FONT, fg=GUESSES_COLOUR)
             guess_label.place(x=359, y=700, width=250, height=50)
             submit_button["state"] = DISABLED
             entry.delete(0, END)
             for i in button_list:
-                i[0].place(x=x_start + i[1] * 70, y=620, width=50, height=50)
+                i[0].place(x=X_BUTTON_START + i[1] * 70, y=620, width=50, height=50)
         else:
-            button_list[0][0].place(x=x_start + button_list[0][1]*70, y=620, width=50, height=50)
+            for b in full_b_list:
+                b["bg"] = NORM_BUTTON_COLOUR
+            button_list[0][0]["bg"] = CURRENT_BUTTON_COLOUR
+            button_list[0][0].place(x=X_BUTTON_START + button_list[0][1]*70, y=620, width=50, height=50)
             image_label.forget()
             image_label = Label(image=img_list[6 - len(button_list)])
             image_label.grid(row=0, column=0, columnspan=3)
@@ -38,8 +61,9 @@ def guess(button_list, x_start, image_label, img_list, entry, guess_label, submi
             guess_label.place(x=359, y=700, width=250, height=50)
             entry.delete(0, END)
     else:
-        guess_label = Label(text=f"The movie was \n{MOVIE_NAME}", font=FONT, fg=GUESSES_COLOUR)
+        guess_label = Label(text=f"The movie was \n{movie}", font=FONT, fg=GUESSES_COLOUR)
         guess_label.place(x=359, y=700, width=250, height=50)
+        submit_button["state"] = DISABLED
 
 
 
