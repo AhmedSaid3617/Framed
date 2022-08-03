ad9a3732c765e42f1df16a613b173cd2533c2924
AhmedSaid3617 <said.ahmed3617@gmail.com>
2022-08-03 18:09:12 +0200

    Added suggestions when guessing the movie, and added more movies.

--- framed.py
+++ framed.py
@@ -1,11 +1,11 @@
-import random
-
 from framed_functions import *
 
 movie_index = 0
 movies = []
 
+
 def main():
+
     global movie_index
     with open("E:\Coding\PythonProjects\Framed\\sol.txt", newline='') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -29,12 +29,19 @@ def main():
     #  List of images
     image_dirs = os.listdir(PICTURE_DIRECTORY) # List of paths to images.
     pictures = [] # List of ImageTk objects.
-    for img in image_dirs:
-        pictures.append(ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\" + img)))
+    """ for img in image_dirs:
+        pictures.append(ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\" + img).resize((806, 436))))"""
 
     #  Label for display of images
-    img = ImageTk.PhotoImage(Image.open(PICTURE_DIRECTORY + "\\001.jpeg"))
-    img_label = Label(image=img)
+    for path in image_dirs:
+        pil_img = Image.open(PICTURE_DIRECTORY + "\\" + path)
+        img_width, img_height = pil_img.size
+        new_width = int(img_width*(5/6))
+        new_height = int(img_height * (5/6))
+        img = ImageTk.PhotoImage(pil_img.resize((new_width, new_height)))
+        pictures.append(img)
+
+    img_label = Label(image=pictures[0])
 
     #  Entry to enter movie name.
     e = Entry(width=50, bd=4, font=FONT)
@@ -57,7 +64,7 @@ def main():
                            command=lambda: guess(buttons_list,
                                                  img_label, pictures,
                                                  e, guesses_label,
-                                                 button_submit, full_buttons_list, MOVIE_NAME))
+                                                 button_submit, full_buttons_list, MOVIE_NAME, list_box, scroll_bar))
 
     button_replay = Button(text="PLay Again", bg=NORM_BUTTON_COLOUR, font=10, activebackground=CLICKED_BUTTON_COLOUR,
                            command=lambda: [root.destroy(), main()])
@@ -68,23 +75,27 @@ def main():
     # Guesses left label
     guesses_label = Label(text="You have 6 guesses left!", font=FONT, fg=GUESSES_COLOUR)
 
+    # Listbox
+    list_box = Listbox(root, height=5, font=FONT)
+    scroll_bar = Scrollbar(root, orient='vertical', command=list_box.yview)
+    list_box['yscrollcommand'] = scroll_bar.set
+    list_box.bind("<<ListboxSelect>>", lambda event: select_movie(e, list_box))
 
     # Display
     #  Label
-    img_label.grid(row=0, column=0, columnspan=3)
+    img_label.place(x=80, y=15)
 
     #  Entry
     e.place(x=134, y=550, width=700, height=50)
+    e.bind("<KeyRelease>", lambda event: search(e, list_box, scroll_bar))
 
     #  Button
     button_submit.place(x=845, y=550, width=80, height=50)
     button_1.place(x=X_BUTTON_START, y=620, width=50, height=50)
-    x_button = X_BUTTON_START
     button_replay.place(x=845, y=620, width=120, height=50)
 
-
     # Guesses label
-    guesses_label.place(x=359, y=700, width=250, height=50)
+    guesses_label.place(x=359, y=700, width=300, height=50)
 
     root.mainloop()
 
--- framed_functions.py
+++ framed_functions.py
@@ -1,9 +1,8 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import os
-import random
 import csv
-
+import time
 
 movie_dirs = os.listdir(os.path.dirname(os.path.realpath(__file__)) + "\images")
 
@@ -15,7 +14,6 @@ GUESSES_COLOUR = "#fc3d8d"
 FONT = "serilic"
 X_BUTTON_START = 284
 
-
 NUMBERS_DICT = {
     1: "first",
     2: "second",
@@ -25,24 +23,68 @@ NUMBERS_DICT = {
     6: "sixth"
 }
 
-guesses = 6
+movie_lst = []
+with open("movies.txt") as file:
+    for row in file:
+        movie_lst.append(row.replace("\n", ''))
+
+
+def search(entry, listbox, scrollbar):
+
+    global result
+    result = []
+    count = 0
+    query = entry.get()
+
+    if query != '':
+        for i in movie_lst:
+            if query.lower() in i.lower():
+                result.append(i)
+
+        if result:
+            for n in range(len(result) - 1):
+
+                if result[n].split()[0].lower().startswith(query.lower()):
+                    result[count], result[n] = result[n], result[count]
+                    count += 1
+
+        result_var = StringVar()
+        result_var.set(result)
+
+        listbox['listvariable'] = result_var
+
+        scrollbar.place(x=819, y=460, height=80, width=15)
+
+        listbox.place(x=134, y=460, width=685, height=90)
+
+    elif query == '':
+        listbox.place(x=0, y=0, width=0)
+        scrollbar.place(x=0, y=0, width=0)
+
+
+def select_movie(entry, listbox):
+    entry.delete(0, END)
+    entry.insert(0, result[int(listbox.curselection()[0])])
 
 
 def switch_img(index, image_label, lst, button, button_list):
     image_label.forget()
     image_label = Label(image=lst[index])
-    image_label.grid(row=0, column=0, columnspan=3)
+    image_label.place(x=80, y=15)
     for b in button_list:
         b["bg"] = NORM_BUTTON_COLOUR
     button["bg"] = CURRENT_BUTTON_COLOUR
 
 
-def guess(button_list, image_label, img_list, entry, guess_label, submit_button, full_b_list, movie):
-    global guesses
+def guess(button_list, image_label, img_list, entry, guess_label, submit_button, full_b_list, movie, listbox, scrollbar):
+
+    guesses = len(button_list) + 1
+
     if button_list or entry.get().lower() == movie.lower():
         if entry.get().lower() == movie.lower():
-            guess_label = Label(text=f"You got it on the {NUMBERS_DICT[7 - guesses]} guess!", font=FONT, fg=GUESSES_COLOUR)
-            guess_label.place(x=359, y=700, width=250, height=50)
+            """guess_label = Label(text=f"You got it on the {NUMBERS_DICT[7 - guesses]} guess!", font=FONT, fg=GUESSES_COLOUR)
+            guess_label.place(x=359, y=700, width=250, height=50)"""
+            guess_label.config(text=f"You got it on the {NUMBERS_DICT[7 - guesses]} guess!")
             submit_button["state"] = DISABLED
             entry.delete(0, END)
             for i in button_list:
@@ -54,16 +96,21 @@ def guess(button_list, image_label, img_list, entry, guess_label, submit_button,
             button_list[0][0].place(x=X_BUTTON_START + button_list[0][1]*70, y=620, width=50, height=50)
             image_label.forget()
             image_label = Label(image=img_list[6 - len(button_list)])
-            image_label.grid(row=0, column=0, columnspan=3)
+            image_label.place(x=80, y=15)
             button_list.pop(0)
             guesses -= 1
-            guess_label = Label(text=f"You have {guesses} guesses left!", font=FONT, fg=GUESSES_COLOUR)
-            guess_label.place(x=359, y=700, width=250, height=50)
+            """guess_label = Label(text=f"You have {guesses} guesses left!", font=FONT, fg=GUESSES_COLOUR)
+            guess_label.place(x=359, y=700, width=250, height=50)"""
+            guess_label.config(text=f"You have {guesses} guesses left!")
             entry.delete(0, END)
     else:
-        guess_label = Label(text=f"The movie was \n{movie}", font=FONT, fg=GUESSES_COLOUR)
-        guess_label.place(x=359, y=700, width=250, height=50)
+        """guess_label = Label(text=f"The movie was \n{movie}", font=FONT, fg=GUESSES_COLOUR)
+        guess_label.place(x=359, y=700, width=250, height=50)"""
+        guess_label.config(text=f"The movie was \n{movie}")
         submit_button["state"] = DISABLED
+
+    listbox.place(x=0, y=0, width=0)
+    scrollbar.place(x=0, y=0, width=0)
 
 
 
